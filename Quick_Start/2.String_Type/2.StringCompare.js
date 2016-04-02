dff341e03956fa48f6e5065e34fe6d0d642328c3
Danny <kiss5891@gmail.com>
2016-04-02 16:12:36 +0800

    Fix SQL.lite

---
+++ Quick_Start/2.String_Type/2.StringCompare.js
@@ -0,0 +1,20 @@
+var s ='I say "good"';            //定義一個s裏面放置I say "good"
+console.log(s);                   //秀出s
+var x = '2'                       //定義x值為字串'2'
+var y = '3'                       //定義y值為字串'3'
+console.log(x+y);                 //秀出兩個字串相加
+console.log(Number(x)+Number(y)); //秀出將兩個字串轉型後相加
+console.log(x<y);                 //x與y比大小
+var a ="a";                       //定義一個a裏面放置字串"a"
+var b ="b";                       //定義一個b裏面放置字串"b"
+console.log(a<b);                 //比較a是否小於b
+console.log(a>b);                 //比較a是否大於b
+//此排序取決於Unicode的順序大小
+/*執行結果
+I say "good"
+23
+5
+true
+true
+false
+*/
