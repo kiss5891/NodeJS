70148e5d203bec551488fd2d21dfaf1084744893
chunwang <q60563@gmail.com>
2016-04-15 15:13:58 +0800

    alter

---
+++ Basic/Data_Type/Dictionaries/Dictionaries.js
@@ -0,0 +1,20 @@
+/*
+Dictionaries是一種使用key/value形式儲存的資料結構
+*/
+
+//Dictionaries
+var dictionary = {				//建立一個字典
+	A : 'apple',				//給予每個key值value
+	B : 'ball',
+	C : 'cat'
+};
+
+console.log(dictionary['C']);			//顯示key為C所對應的value
+console.log(dictionary['B']);			//顯示key為B所對應的value
+console.log(dictionary['A']);			//顯示key為A所對應的value
+//--------------------------------------
+/*
+cat
+ball
+apple
+*/
